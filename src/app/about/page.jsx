998a9456b360dcaca4fde4976b101ddda4adad30
Utsav Agrawal <agrutsav88728@gmail.com>
2024-04-16 21:44:38 +0530

    Created About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Three Idiots is my favourite film. This is the story of three Imperial
+        College of Engineering engineering students who discover their
+        relationship by making themselves look like idiots and learning from it.
+        Farhan Qureshi, played by R. Madhavan, Raju Rastogi, played by Sharman
+        Joshi, and Ranchoddas Shamaldas “Rancho” Chanchad, played by Aamir Khan,
+        were the key characters. The major reason I enjoy this picture is that,
+        although being a comedy, it has a message and it is about mankind. Aside
+        from that, I enjoy this film since it offers information about our
+        educational system.
+      </p>
+      <p>
+        This film has had a huge impact on me, especially the statement "All is
+        well" that Rancho constantly says. I think that whatever conditions may
+        arise if any obstacles arise, I will simply state this and remain cool
+        because I believe that every problem must have a solution.
+      </p>
+    </div>
+  );
+}
